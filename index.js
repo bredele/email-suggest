9fb936b2bfd2852ceb22a6aa43de2dd155a89632
bredele <olivier.wietrich@gmail.com>
2018-12-13 11:08:59 -0700

    Create email address based on list of popular domains

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+/**
+ * Dependencies
+ */
+
+const popular = require('email-domain-popular')
+const email = require('email-domain')
+const pick = require('array-prefix')
 
 module.exports = (address) => {
-  
+  const [left, right] = address.split('@')
+  if (right != null) {
+    const domains = pick(right, popular)
+    if (domains.length < 2) return email(address, domains[0])
+  }
 }
