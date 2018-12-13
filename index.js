ff3faee428533c80e7f26332ac29933a83d9a41b
bredele <olivier.wietrich@gmail.com>
2018-12-13 11:24:17 -0700

    Refactor code

--- index.js
+++ index.js
@@ -6,7 +6,24 @@ const popular = require('email-domain-popular')
 const email = require('email-domain')
 const pick = require('array-prefix')
 
-module.exports = (address, cb = (str, domains) => email(str, domains[0])) => {
+/**
+ * Default suggestion mapping function.
+ * By default return the first suggestion in the list of popular domains.
+ *
+ * @type {Function}
+ */
+
+const map = (str, domains) => email(str, domains[0])
+
+/**
+ * Suggest email address based on list of popular domains.
+ *
+ * @param {String} address
+ * @param {Function?} map
+ * @api public
+ */
+
+module.exports = (address, cb = map) => {
   const [left, right] = address.split('@')
   if (right != null) return cb(address, pick(right, popular), left)
 }
